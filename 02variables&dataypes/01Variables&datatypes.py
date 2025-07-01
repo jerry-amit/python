768cf92646429635e07917e3d766a44a8cb4850c
jerry <jerry0219038@gmail.com>
2025-07-01 12:49:38 +0530

    Add files via upload

---
+++ 02variables&dataypes/01Variables&datatypes.py
@@ -0,0 +1,12 @@
+        #VARIABLES AND DATATYPES
+print("helo world in python")
+
+a = 10;    #variable
+b = 10;
+c = "jerry" #keywords
+
+name ="jerry";
+
+print(a + b)
+
+print(name)
---
+++ 02variables&dataypes/02_datatypes.py
@@ -0,0 +1,15 @@
+
+a = 1 #a is an integer
+
+b = 5.22 # b is a floating point number
+
+c = "jerry" # c is a String
+
+d = False  # d is a boolean variable
+
+e = None # e is  a none type variable
+
+if j>k
+
+
+    
