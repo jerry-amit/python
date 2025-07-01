7e3f448e01a7c65c71006ca0d1dcf1b7c0443f9c
jerry <jerry0219038@gmail.com>
2025-07-01 14:44:56 +0530

    Add files via upload

---
+++ PRACTICE SET/CHAPTER1/TWINKLE.py
@@ -0,0 +1,5 @@
+#  YOU HAVE TO PRINT THE POEM OF TWINKLE TWINKLE
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.""")
---
+++ PRACTICE SET/CHAPTER1/table.py
@@ -0,0 +1,31 @@
+# # USE REPL AND PRINT THE TABLE OF USING IT
+# # import loop
+# i=5;
+# count =1;
+# print(i*count)
+
+# print(i*(count*2))
+# print(i*(count*3))
+# print(i*(count*4))
+# print(i*(count*5))
+# print(i*(count*6))
+# print(i*(count*7))
+# print(i*(count*8))
+# print(i*(count*9))
+# print(i*(count*10))
+
+# USE REPL AND PRINT THE TABLE OF USING IT
+# import loop
+i=5;
+
+print(i,"* 1 = ",i*1);
+print(i,"* 2 = ",i*2);
+print(i,"* 3 = ",i*3);
+print(i,"* 4 = ",i*4);
+print(i,"* 5 = ",i*5);
+print(i,"* 6 = ",i*6);
+print(i,"* 7 = ",i*7);
+print(i,"* 8 = ",i*8);
+print(i,"* 9 = ",i*9);
+print(i,"*1 0 = ",i*10);
+
