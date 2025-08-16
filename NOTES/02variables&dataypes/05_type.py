4454382921e458ed2818e5d5a831ab78c8cfdbac
jerry <jerry0219038@gmail.com>
2025-08-16 22:08:54 +0530

    Add files via upload

---
+++ NOTES/02variables&dataypes/05_type.py
@@ -0,0 +1,42 @@
+# a = 31
+# t = type(a) # class <int>
+# print(t)
+
+# a = 31.2
+# t = type(a) # class <float>
+# print(t)
+ 
+ 
+#  b = "harry"
+# c = type(b) # class <string>
+# print(c)
+
+
+a = "31.1"
+b = float(a)    # a but the type should be float
+t = type(b) # class <float>
+print(t)
+print(b)
+
+        # integer to string  conversion
+
+a = 31 
+b = str(a)   # a but the type should be string
+t=type(b)       # class <string>
+print(t)
+
+
+       # string to integer  conversion
+
+a = "31"        # a but the type should be int
+b = int(a)      # class(int)
+t=type(b)
+print(t)
+
+
+   #  integer to float  conversion
+
+a = 31        # a but the type should be float
+b = float(a)      # class(float)
+t=type(b)
+print(t)
---
+++ NOTES/02variables&dataypes/06_input.py
@@ -0,0 +1,11 @@
+a = int(input("Enter number 1 :"))  #a = input("Enter number 1 :")
+
+                                #in this case answer is string type 
+                                # to perform addtion use type conversion
+
+b=int(input("Enter number 2 :"))
+
+print("Number a is :",a)
+print("Number b is :",b)
+
+print(" sum is ",a + b)
