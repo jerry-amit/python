29743173054330b936ee5f420c9e924afdedac87
jerry <jerry0219038@gmail.com>
2025-08-16 22:07:17 +0530

    04_opertor.py

--- NOTES/02variables&dataypes/04_opertor.py
+++ NOTES/02variables&dataypes/04_opertor.py
@@ -1,22 +1,56 @@
-  # ARITHMATIC OPERATORS
-  
-a = 7
-  
-b = 4
-  
-c = a+b
-  
-print(c)
-  
-    
-#ASSIGNMENT OPERATOR
+# arithmatic operators
+ 
+a=34
+ b=4
+ c=a+b
 
-a = 4-2 # Assign 4-2 in a 
+ print(c)
 
-print(a)
-b = 6
 
-#b += 3  # increment the value of b  by 3 and then assign it to b
-b -= 3  # increment the value of b  by 3 and then assign it to b
+# Assignment operAtors
 
+a = 34;
+ b=6
+ b+3= #increment the value of  b by 3 and then assign it to b
+ b-3= #decrement the value of  b by 3 and then assign it to b
 print(b)
+
+
+# COMPARISON OPERATORS
+
+d= 5<4  #returns answers in always true or false
+d= 5>4  #returns answers in always true or false
+d= 5<=4  #returns answers in always true or false
+d= 5!=4  #returns answers in always true or false
+d= 5==5  #returns answers in always true or false
+print(d)
+
+
+# LOGICAL OPERATORS
+
+
+e = true or false
+print(e)
+
+            # Truth table for OR operators
+print("True or False is ", true or false)
+print("True or true is ", true or true)
+print("false or true is ", false or true)
+print("false or False is ",false or false)
+ 
+ 
+            # Truth table for AND operators
+print("True and False is ", true and false)
+print("True and true is ", true and true)
+print("false and true is ", false and true)
+print("false and False is ",false and false)
+ 
+            # Truth table for NOT operators
+print("True and not False is ", true and not false)
+print("True and not true is ", true and not true)
+print("false and not true is ", false and not true)
+print("false and not False is ",false and not false)
+
+print(not(false))
+print(not(true))
+ 
