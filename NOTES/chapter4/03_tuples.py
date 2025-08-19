0ae4d1f3630c619e943047502ac0ec4a35cf608e
jerry <jerry0219038@gmail.com>
2025-08-19 18:19:05 +0530

    Add files via upload

---
+++ NOTES/chapter4/03_tuples.py
@@ -0,0 +1,11 @@
+a=(1,2,3,4,5,6)
+print(a)
+print(type(a))
+
+b=()
+print(type(b))
+c=("amit","jerry")
+print(type(c))
+
+d=(1,)
+print(type(d))
---
+++ NOTES/chapter4/04_tuples_methods.py
@@ -0,0 +1,29 @@
+a=(1,45,45,342,34245,6,"amit","jerry")
+print(a)
+print(type(a))
+
+no=a.count(45)# To count how many times it will repeat
+print(no)
+
+
+i=a.index(342)
+print(i)
+
+tuple1 =(1,2,3,4,5)
+tuple2 =(11,21,31,4,45,56)
+
+concatenated =(tuple1+tuple2) #Add two tuples or more
+print(concatenated)
+
+
+tuple = (1,2,3)
+repeated = tuple*3# To repeat the Tuples element list
+print(repeated)
+
+tuple=(1,2,3)
+print(2 in tuple) # To check list element of tuple is available or not
+print(len(tuple))
+
+tuple=(1,2,3)
+a,b,c = tuple #
+print(a,b,c)
