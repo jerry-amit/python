a0f2bd077ba4be842a672b016ddc82072c669e9b
jerry <jerry0219038@gmail.com>
2025-08-20 13:44:32 +0530

    Add files via upload

---
+++ PRACTICE SET/chapter 4/04_problem.py
@@ -0,0 +1,5 @@
+#Write a program to sum a List with 4 numbers.
+
+l=[1,6,5,5]
+
+print(sum(l))
---
+++ PRACTICE SET/chapter 4/05_problem.py
@@ -0,0 +1,6 @@
+#Write a Program to Count the numbers of zeros in the following tuple
+
+a=(7,0,8,0,0,0,9,7)
+
+n = a.count(0)
+print(n)
