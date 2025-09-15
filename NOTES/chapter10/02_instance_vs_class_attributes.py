e8b77ec6f647916dbdd3b04ac6c15829fc96afe2
jerry <jerry0219038@gmail.com>
2025-09-15 11:04:11 +0530

    Delete NOTES/chapter10 directory

--- NOTES/chapter10/02_instance_vs_class_attributes.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    name="Jerry" #this is a class atrribute
-    language = "py"
-    salary = "120000"
-    
-jerry = Employee()
-jerry.language = "javascript" # instance attributes are more valuable than class attributes
-print(jerry.name,jerry.language,jerry.salary)
