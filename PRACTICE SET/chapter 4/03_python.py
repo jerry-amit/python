cd301256323bddc824625e4155565eaf57d4ab3b
jerry <jerry0219038@gmail.com>
2025-09-15 11:05:22 +0530

    Delete PRACTICE SET/chapter 4 directory

--- PRACTICE SET/chapter 4/03_python.py
+++
@@ -1,5 +0,0 @@
-# Check that a type cannot be changed in python
-
-a=(31,4,5,6,"jerry")
-
-a[2] ="larry" # it cannot change because it it immutable
--- PRACTICE SET/chapter 4/04_problem.py
+++
@@ -1,5 +0,0 @@
-#Write a program to sum a List with 4 numbers.
-
-l=[1,6,5,5]
-
-print(sum(l))
